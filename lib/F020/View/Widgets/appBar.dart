031a2f447235e6a86830aaf5bc504a9c454004fd
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-05-27 22:58:00 +0200

    upd

--- lib/F020/View/Widgets/appBar.dart
+++ lib/F020/View/Widgets/appBar.dart
@@ -1,12 +1,12 @@
 //require all arguments you want
 
-// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, sized_box_for_whitespace
+// ignore_for_file: non_constant_identifier_names, prefer_const_constructors, sized_box_for_whitespace, file_names
 
 import 'package:flutter/material.dart';
 
 // Costume AppBar -------------------------------------------------------------
 
-AppBar C_AppBar() {
+AppBar C_AppBar({required context}) {
   return AppBar(
     titleSpacing: 00.0,
     // centerTitle: true,
@@ -16,36 +16,36 @@ AppBar C_AppBar() {
     elevation: 0.00,
     backgroundColor: Colors.transparent,
     title: Container(
-      margin: EdgeInsets.only(top: 20),
-      height: 50,
+      margin: EdgeInsets.only(
+          top: 20,
+          left: MediaQuery.of(context).size.width / 10,
+          right: MediaQuery.of(context).size.width / 10),
+      height: 90,
       width: double.infinity,
-      child: Row(
-        children: [
-          Expanded(
-            child: Container(
-              height: 50,
-              child: Expanded(
-                child: Image.asset(
-                  'assets/images/tawazun-logo.png',
-                  // fit: BoxFit.cover,
-                ),
+      child: Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(left: 40),
+              height: 60,
+              width: MediaQuery.of(context).size.width / 4,
+              child: Image.asset(
+                'assets/images/tawazun-logo.png',
               ),
             ),
-          ),
-          Expanded(
-            child: Center(
-              child: Container(
-                margin: const EdgeInsets.only(left: 10, right: 70),
-                alignment: Alignment.center,
-                height: double.infinity,
-                child: const Text(
-                  'Patient Confidentiality Statement',
-                  style: TextStyle(fontSize: 18, color: Colors.black),
-                ),
+            Container(
+              margin: const EdgeInsets.only(left: 30),
+              alignment: Alignment.centerLeft,
+              height: double.infinity,
+              width: MediaQuery.of(context).size.width / 2.5,
+              child: const Text(
+                'Patient Confidentiality Statement',
+                style: TextStyle(fontSize: 18, color: Colors.black),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ),
   );
