d9289d1ec5131a271588649b75fa4fd033b7b75c
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-05-28 19:25:38 +0200

    yes

---
+++ lib/form_011_To_023/F023/Controller/controller.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: camel_case_types, non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class Controler_F020 extends GetxController {
+  final name_controller = TextEditingController();
+  final Badge_controller = TextEditingController();
+  final Signature_controller = TextEditingController();
+  final Date_Signature_controller = TextEditingController();
+  final THHCManager_controller = TextEditingController();
+  final Date_THHCManage_controller = TextEditingController();
+}
