e8f5361b7e734d8453e967f1ea80ada6e8769add
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-05-31 23:03:58 +0200

    upd

---
+++ lib/form_011_To_023/F021/Controller/controller.dart
@@ -0,0 +1,141 @@
+// ignore_for_file: camel_case_types, non_constant_identifier_names
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class Controler_F021 extends GetxController {
+  final one_row_1 = TextEditingController();
+  final one_row_2 = TextEditingController();
+  final one_row_3 = TextEditingController();
+  final one_row_4 = TextEditingController();
+  final one_row_5 = TextEditingController();
+  final one_row_6 = TextEditingController();
+  final one_row_7 = TextEditingController();
+  final one_row_8 = TextEditingController();
+
+  final tow_row_1 = TextEditingController();
+  final tow_row_2 = TextEditingController();
+  final tow_row_3 = TextEditingController();
+  final tow_row_4 = TextEditingController();
+  final tow_row_5 = TextEditingController();
+  final tow_row_6 = TextEditingController();
+  final tow_row_7 = TextEditingController();
+  final tow_row_8 = TextEditingController();
+
+  final three_row_1 = TextEditingController();
+  final three_row_2 = TextEditingController();
+  final three_row_3 = TextEditingController();
+  final three_row_4 = TextEditingController();
+  final three_row_5 = TextEditingController();
+  final three_row_6 = TextEditingController();
+  final three_row_7 = TextEditingController();
+  final three_row_8 = TextEditingController();
+
+  final four_row_1 = TextEditingController();
+  final four_row_2 = TextEditingController();
+  final four_row_3 = TextEditingController();
+  final four_row_4 = TextEditingController();
+  final four_row_5 = TextEditingController();
+  final four_row_6 = TextEditingController();
+  final four_row_7 = TextEditingController();
+  final four_row_8 = TextEditingController();
+
+  final five_row_1 = TextEditingController();
+  final five_row_2 = TextEditingController();
+  final five_row_3 = TextEditingController();
+  final five_row_4 = TextEditingController();
+  final five_row_5 = TextEditingController();
+  final five_row_6 = TextEditingController();
+  final five_row_7 = TextEditingController();
+  final five_row_8 = TextEditingController();
+
+  final sex_row_1 = TextEditingController();
+  final sex_row_2 = TextEditingController();
+  final sex_row_3 = TextEditingController();
+  final sex_row_4 = TextEditingController();
+  final sex_row_5 = TextEditingController();
+  final sex_row_6 = TextEditingController();
+  final sex_row_7 = TextEditingController();
+  final sex_row_8 = TextEditingController();
+
+  final seven_row_1 = TextEditingController();
+  final seven_row_2 = TextEditingController();
+  final seven_row_3 = TextEditingController();
+  final seven_row_4 = TextEditingController();
+  final seven_row_5 = TextEditingController();
+  final seven_row_6 = TextEditingController();
+  final seven_row_7 = TextEditingController();
+  final seven_row_8 = TextEditingController();
+
+  final eight_row_1 = TextEditingController();
+  final eight_row_2 = TextEditingController();
+  final eight_row_3 = TextEditingController();
+  final eight_row_4 = TextEditingController();
+  final eight_row_5 = TextEditingController();
+  final eight_row_6 = TextEditingController();
+  final eight_row_7 = TextEditingController();
+  final eight_row_8 = TextEditingController();
+
+  final nine_row_1 = TextEditingController();
+  final nine_row_2 = TextEditingController();
+  final nine_row_3 = TextEditingController();
+  final nine_row_4 = TextEditingController();
+  final nine_row_5 = TextEditingController();
+  final nine_row_6 = TextEditingController();
+  final nine_row_7 = TextEditingController();
+  final nine_row_8 = TextEditingController();
+
+  final ten_row_1 = TextEditingController();
+  final ten_row_2 = TextEditingController();
+  final ten_row_3 = TextEditingController();
+  final ten_row_4 = TextEditingController();
+  final ten_row_5 = TextEditingController();
+  final ten_row_6 = TextEditingController();
+  final ten_row_7 = TextEditingController();
+  final ten_row_8 = TextEditingController();
+
+  final eleven_row_1 = TextEditingController();
+  final eleven_row_2 = TextEditingController();
+  final eleven_row_3 = TextEditingController();
+  final eleven_row_4 = TextEditingController();
+  final eleven_row_5 = TextEditingController();
+  final eleven_row_6 = TextEditingController();
+  final eleven_row_7 = TextEditingController();
+  final eleven_row_8 = TextEditingController();
+
+  final twelve_row_1 = TextEditingController();
+  final twelve_row_2 = TextEditingController();
+  final twelve_row_3 = TextEditingController();
+  final twelve_row_4 = TextEditingController();
+  final twelve_row_5 = TextEditingController();
+  final twelve_row_6 = TextEditingController();
+  final twelve_row_7 = TextEditingController();
+  final twelve_row_8 = TextEditingController();
+
+  final thirteen_row_1 = TextEditingController();
+  final thirteen_row_2 = TextEditingController();
+  final thirteen_row_3 = TextEditingController();
+  final thirteen_row_4 = TextEditingController();
+  final thirteen_row_5 = TextEditingController();
+  final thirteen_row_6 = TextEditingController();
+  final thirteen_row_7 = TextEditingController();
+  final thirteen_row_8 = TextEditingController();
+
+  final fourteen_row_1 = TextEditingController();
+  final fourteen_row_2 = TextEditingController();
+  final fourteen_row_3 = TextEditingController();
+  final fourteen_row_4 = TextEditingController();
+  final fourteen_row_5 = TextEditingController();
+  final fourteen_row_6 = TextEditingController();
+  final fourteen_row_7 = TextEditingController();
+  final fourteen_row_8 = TextEditingController();
+
+  final fifteen_row_1 = TextEditingController();
+  final fifteen_row_2 = TextEditingController();
+  final fifteen_row_3 = TextEditingController();
+  final fifteen_row_4 = TextEditingController();
+  final fifteen_row_5 = TextEditingController();
+  final fifteen_row_6 = TextEditingController();
+  final fifteen_row_7 = TextEditingController();
+  final fifteen_row_8 = TextEditingController();
+}
