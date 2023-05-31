6de7a39358d0414c20b8b598c57f70a70024910a
Eng-AkramShaker <akramaboubakersh@gmail.com>
2023-06-01 01:16:15 +0200

    upd-F018

---
+++ lib/form_011_To_023/F018/Controller/controller.dart
@@ -0,0 +1,119 @@
+// ignore_for_file: camel_case_types, non_constant_identifier_names
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+
+class Controler_F021 extends GetxController {
+  final Staff = TextEditingController();
+  final id = TextEditingController();
+  final Date_1 = TextEditingController();
+  final Joining = TextEditingController();
+
+  final COMPLETED_1 = TextEditingController();
+  final COMMENTS_1 = TextEditingController();
+  final COMPLETED_2 = TextEditingController();
+  final COMMENTS_2 = TextEditingController();
+  final COMPLETED_3 = TextEditingController();
+  final COMMENTS_3 = TextEditingController();
+  final COMPLETED_4 = TextEditingController();
+  final COMMENTS_4 = TextEditingController();
+  final COMPLETED_5 = TextEditingController();
+
+  final COMMENTS_5 = TextEditingController();
+  final COMPLETED_6 = TextEditingController();
+  final COMMENTS_6 = TextEditingController();
+  final COMPLETED_7 = TextEditingController();
+  final COMMENTS_7 = TextEditingController();
+  final COMPLETED_8 = TextEditingController();
+  final COMMENTS_8 = TextEditingController();
+  final COMPLETED_9 = TextEditingController();
+  final COMMENTS_9 = TextEditingController();
+  final COMPLETED_10 = TextEditingController();
+
+  final COMMENTS_10 = TextEditingController();
+  final COMPLETED_11 = TextEditingController();
+  final COMMENTS_11 = TextEditingController();
+
+  final COMPLETED_12 = TextEditingController();
+  final COMMENTS_12 = TextEditingController();
+  final COMPLETED_13 = TextEditingController();
+  final COMMENTS_13 = TextEditingController();
+  final COMPLETED_14 = TextEditingController();
+  final COMMENTS_14 = TextEditingController();
+
+  final COMPLETED_15 = TextEditingController();
+  final COMMENTS_15 = TextEditingController();
+  final COMPLETED_16 = TextEditingController();
+  final COMMENTS_16 = TextEditingController();
+  final COMPLETED_17 = TextEditingController();
+  final COMMENTS_17 = TextEditingController();
+  final COMPLETED_18 = TextEditingController();
+  final COMMENTS_18 = TextEditingController();
+
+  final COMPLETED_19 = TextEditingController();
+  final COMMENTS_19 = TextEditingController();
+  final COMPLETED_20 = TextEditingController();
+  final COMMENTS_20 = TextEditingController();
+  final COMPLETED_21 = TextEditingController();
+  final COMMENTS_21 = TextEditingController();
+  final COMPLETED_22 = TextEditingController();
+  final COMMENTS_22 = TextEditingController();
+  final COMPLETED_23 = TextEditingController();
+  final COMMENTS_23 = TextEditingController();
+  final COMPLETED_24 = TextEditingController();
+  final COMMENTS_24 = TextEditingController();
+  final COMPLETED_25 = TextEditingController();
+  final COMMENTS_25 = TextEditingController();
+  final COMPLETED_26 = TextEditingController();
+  final COMMENTS_26 = TextEditingController();
+  final COMPLETED_27 = TextEditingController();
+  final COMMENTS_27 = TextEditingController();
+  final COMPLETED_28 = TextEditingController();
+  final COMMENTS_28 = TextEditingController();
+  final COMPLETED_29 = TextEditingController();
+  final COMMENTS_29 = TextEditingController();
+  final COMPLETED_30 = TextEditingController();
+  final COMMENTS_30 = TextEditingController();
+
+  final COMPLETED_31 = TextEditingController();
+  final COMMENTS_31 = TextEditingController();
+  final COMPLETED_32 = TextEditingController();
+  final COMMENTS_32 = TextEditingController();
+  final COMPLETED_33 = TextEditingController();
+  final COMMENTS_33 = TextEditingController();
+  final COMPLETED_34 = TextEditingController();
+  final COMMENTS_34 = TextEditingController();
+  final COMPLETED_35 = TextEditingController();
+  final COMMENTS_35 = TextEditingController();
+  final COMPLETED_36 = TextEditingController();
+  final COMMENTS_36 = TextEditingController();
+  final COMPLETED_37 = TextEditingController();
+  final COMMENTS_37 = TextEditingController();
+  final COMPLETED_38 = TextEditingController();
+  final COMMENTS_38 = TextEditingController();
+  final COMPLETED_39 = TextEditingController();
+  final COMMENTS_39 = TextEditingController();
+  final COMPLETED_40 = TextEditingController();
+  final COMMENTS_40 = TextEditingController();
+  final COMPLETED_41 = TextEditingController();
+  final COMMENTS_41 = TextEditingController();
+  final COMPLETED_42 = TextEditingController();
+  final COMMENTS_42 = TextEditingController();
+
+  final COMPLETED_43 = TextEditingController();
+  final COMMENTS_43 = TextEditingController();
+  final COMPLETED_44 = TextEditingController();
+  final COMMENTS_44 = TextEditingController();
+  final COMPLETED_45 = TextEditingController();
+  final COMMENTS_45 = TextEditingController();
+
+  final Employee = TextEditingController();
+  final Date_2 = TextEditingController();
+  final Preceptor = TextEditingController();
+  final Date_3 = TextEditingController();
+  final Director = TextEditingController();
+  final Date_4 = TextEditingController();
+
+  final STAFF_NAME_SIGNATURE = TextEditingController();
+  final THHC_MANAGER_NAME_SIGNATURE = TextEditingController();
+}
